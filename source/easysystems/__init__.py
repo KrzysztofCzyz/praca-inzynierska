583a752a3419d4b470a75601597173899e4f158d
Krzysztof Czyz <czyz.krzysztof98@gmail.com>
2020-10-12 19:34:16 +0200

    Dzien 8 - fixed init

--- source/easysystems/__init__.py
+++ source/easysystems/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-# from flask_mail import Mail
 from source.easysystems.config import Config
 
 
@@ -11,7 +10,6 @@ bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.login_message_category = 'info'
 login_manager.login_message = "Zaloguj się aby uzyskać dostęp do tej strony."
-# mail = Mail()
 
 
 def create_app(config_class=Config):
@@ -21,7 +19,6 @@ def create_app(config_class=Config):
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-    # mail.init_app(app)
 
     from source.easysystems.errors.handlers import errors
     from source.easysystems.main.routes import main
