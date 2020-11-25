728fc9405c819bc3f343b8d434c3b15f1b8e18e1
Krzysztof Czyz <czyz.krzysztof98@gmail.com>
2020-11-25 21:55:31 +0100

    Fixed nasty error handling bug with NGINX server

--- source/easysystems/errors/handlers.py
+++ source/easysystems/errors/handlers.py
@@ -16,3 +16,8 @@ def error_403(error):
 @errors.app_errorhandler(500)
 def error_500(error):
     return render_template('errors/500.html'), 500
+
+
+@errors.app_errorhandler(401)
+def error_401(error):
+    return render_template('errors/403.html'), 401
